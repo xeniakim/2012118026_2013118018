553292e92f582e73be26d409883dd787c03f12a5
cookieshake <cookieshake@hotmail.com>
2016-12-04 17:36:02 +0900

    Add ConditionBuilder.java
    ConditionBuilder.java makes building a WHERE clause easy.

---
+++ src/tool/ConditionBuilder.java
@@ -0,0 +1,85 @@
+package tool;
+
+import java.util.Scanner;
+
+//WHERE에 들어갈 조건문을 만들어 주는 클래스 new ConditionBuilder().ask()로 사용
+public class ConditionBuilder {
+	private StringBuilder output = new StringBuilder();
+	
+	//column을 골라보아
+	private final String COLUMNQUESTION = "Please specify the column which you want to make condition(Press enter : skip) : ";
+	//연산자를 골라보아
+	private final String OPSQUESTION = "Please specify the condition (1: =, 2: >, 3: < , 4: >=, 5: <=, 6: !=) : ";
+	//어떤 값이랑 비교할거니?
+	private final String VALUEQUESTION = "Please specify the condition value (%s ?) : ";
+	//더 할거니?
+	private final String BOOLOPSQUESTION = "Please specify the condition (1: AND, 2: OR, 3: finish) : ";
+	
+	public String ask() {
+		
+		boolean finished = false;
+		
+		while (!finished) {
+			Scanner sc = new Scanner(System.in);
+			
+			try {
+				System.out.print(this.COLUMNQUESTION);
+				String opsAnswer = sc.nextLine();
+				if (opsAnswer.isEmpty()) {
+					sc.close();
+					return null;
+				}
+					 
+				
+				output.append(String.format("\"%s\" ", opsAnswer));
+				
+				System.out.print(this.OPSQUESTION);
+				switch (sc.next()) {
+				case "1":
+					output.append("= ");
+					break;
+				case "2":
+					output.append("> ");
+					break;
+				case "3":
+					output.append("< ");
+					break;
+				case "4":
+					output.append(">= ");
+					break;
+				case "5":
+					output.append("<= ");
+					break;
+				case "6":
+					output.append("!= ");
+					break;
+				default:
+					break;
+				}
+				
+				System.out.print(String.format(this.VALUEQUESTION,
+						this.output.toString().replaceAll("'", "").replaceAll("\"", "")));
+				
+				this.output.append(String.format("'%s' ", sc.next()));
+				
+				System.out.print(this.BOOLOPSQUESTION);
+				switch (sc.next()) {
+				case "1":
+					this.output.append("AND ");
+					break;
+				case "2":
+					this.output.append("OR ");
+					break;
+				case "3":
+					finished = true;
+					break;
+				}
+			} finally {
+				sc.close();
+			}
+		} 
+		
+		return this.output.toString();
+	}
+	
+}
