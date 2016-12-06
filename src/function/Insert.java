d90243b271c7e9676c854e6b88be8602efd78be8
cookieshake <cookieshake@hotmail.com>
2016-12-06 20:55:29 +0900

    Add insert function

---
+++ src/function/Insert.java
@@ -0,0 +1,42 @@
+package function;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import source.MakeConnection;
+import source.PostgresConnector;
+import tool.sql_builder.SQLBuilder;
+
+public class Insert {
+	public Insert() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Please specify the table name : ");
+		String table_name = sc.nextLine();
+		
+		System.out.print("Please specify all columns in order of which you want to insert : ");
+		String c_input = sc.nextLine();
+		String[] columns = c_input.replaceAll(" ", "").split(",");
+		
+		System.out.print("Please specify values for each column : ");
+		String[] values = sc.nextLine().replaceAll(" ", "").split(",");
+		
+		String sql = SQLBuilder
+				.insert_into(table_name)
+				.columns(columns)
+				.values(values)
+				.build();
+		
+		MakeConnection mc = new MakeConnection();
+		mc.takeConnectionInfo();
+		mc.Connect();
+		
+		PostgresConnector pc = new PostgresConnector(mc);
+		try {
+			int result = pc.executeInsert(sql);
+			System.out.print(String.format("<%s rows inserted>", result));
+		} catch (SQLException e) {
+			System.out.println("<0 row inserted due to error>");
+		}
+	}
+}
